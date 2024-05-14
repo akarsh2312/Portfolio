9730378bcbe870e929e6bf048a8e703fc6c214b8
Akarsh Khandelwal <khandelwalakarsh8@gmail.com>
2024-05-15 03:54:27 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Akarsh</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer proficient in MERN Stack and currently looking for good opportunities in this field.
+        </p>
+        <a href="mailto:khandelwalakarsh8@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/Akarsh.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
